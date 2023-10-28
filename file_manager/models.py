7a1d0949e6e9f75ae78f437b560934b7b9583bfb
Dmitrii <dima12sik@yandex.ru>
2023-10-28 18:57:09 +0300

    added File models

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class File(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='дата загрузки')
+    processed = models.BooleanField(default=False, verbose_name='Обработка файла')
+    file = models.FileField(upload_to='files/')
+
+    def __str__(self):
+        return f'Файл загружен {self.uploaded_at}'
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
+        ordering = ('-uploaded_at',)
